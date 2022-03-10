624a8bd8219173642826515fd807aacbac832f72
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-10 17:03:15 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import User from "./components/User";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <h1>User</h1>
+
+            <User
+                first_name="Roby"
+                last_name="Daniele"
+                email="Roby@gmail.com"
+                password="pass"
+                confirm_password="conf_pass"
+            />
+        </>
+    );
 }
 
 export default App;
