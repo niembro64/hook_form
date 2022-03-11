6efc24d15ee30f40f51fa5f064f4d96726bb705e
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-11 00:25:33 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import User from "./components/User";
+import UserForm from "./components/UserForm";
 
 function App() {
     return (
         <>
-            <h1>User</h1>
-
-            <User
-                first_name="Roby"
-                last_name="Daniele"
-                email="Roby@gmail.com"
-                password="pass"
-                confirm_password="conf_pass"
-            />
+            <UserForm />
         </>
     );
 }
